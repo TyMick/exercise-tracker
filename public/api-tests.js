d5636e8f256e128a8f55b38f520d81d259315293
Glitch (ty-exercise-tracker) <none>
2020-02-01 19:48:43 +0000

    Add loading function to buttons

--- public/api-tests.js
+++ public/api-tests.js
@@ -1,39 +1,71 @@
 $(function() {
   $("#createNewUser").submit(function() {
     event.preventDefault();
+    $("button").attr("disabled", true);
+    $("button", this).html(
+      '<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span><span class="sr-only">Uploading...</span>'
+    );
     $.ajax({
       url: "/api/exercise/new-user",
       type: "post",
       data: getFormDataObject("createNewUser"),
-      success: displayResult
+      success: function(result) {
+        displayResult(result);
+        $("#createNewUser button").html("POST");
+        $("button").removeAttr("disabled");
+      }
     });
   });
 
   $("#getAllUsers").submit(function() {
     event.preventDefault();
+    $("button").attr("disabled", true);
+    $("button", this).html(
+      '<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span><span class="sr-only">Uploading...</span>'
+    );
     $.ajax({
       url: "/api/exercise/users",
       type: "get",
-      success: displayResult
+      success: function(result) {
+        displayResult(result);
+        $("#getAllUsers button").html("GET");
+        $("button").removeAttr("disabled");
+      }
     });
   });
 
   $("#addExercise").submit(function() {
     event.preventDefault();
+    $("button").attr("disabled", true);
+    $("button", this).html(
+      '<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span><span class="sr-only">Uploading...</span>'
+    );
     $.ajax({
       url: "/api/exercise/add",
       type: "post",
       data: getFormDataObject("addExercise"),
-      success: displayResult
+      success: function(result) {
+        displayResult(result);
+        $("#addExercise button").html("POST");
+        $("button").removeAttr("disabled");
+      }
     });
   });
 
   $("#getLog").submit(function() {
     event.preventDefault();
+    $("button").attr("disabled", true);
+    $("button", this).html(
+      '<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span><span class="sr-only">Uploading...</span>'
+    );
     $.ajax({
       url: "/api/exercise/log?" + $("#getLog").serialize(),
       type: "get",
-      success: displayResult
+      success: function(result) {
+        displayResult(result);
+        $("#getLog button").html("GET");
+        $("button").removeAttr("disabled");
+      }
     });
   });
 
